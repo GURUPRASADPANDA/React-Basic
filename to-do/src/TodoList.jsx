1a932048076c171a1147d4d742ca38c6ecb0c0ed
mujhepadhnahai <mujhepadhnahe@gmail.com>
2024-11-29 20:29:29 +0530

    new file:   to-do/vite.config.js

---
+++ to-do/src/TodoList.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react';
+export default function TodoList(){
+
+    let[todos,setTodos] = useState(["Sample Task"]);
+    let[newTodo,setnewTodo] = useState("");
+
+    let addNewTask = () => {
+       setTodos([...todos,newTodo])
+    };
+
+    let updateTodoValue= (event)=>{
+      setnewTodo(event.target.value);
+    };
+
+    return (
+        <div>
+           <input type="text" placeholder="Add tasks here" onChange={updateTodoValue}/>
+           <br/><br/>
+           <button onClick={addNewTask}>Add Task</button>
+           <br/><br/><br/><br/>
+
+           <hr></hr>
+           <h3>Tasks to-do</h3>
+           <ul>
+            {
+                todos.map((todo)=>(
+                 <li>{todo}</li>
+                ))
+            }
+           </ul>
+        
+        </div>
+       
+    );
+}
